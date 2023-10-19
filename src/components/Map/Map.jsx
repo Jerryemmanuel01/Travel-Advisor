adb65115c02606763d13acd37a3e473c68f0a31f
Jerryemmanuel01 <jerryemmanuelolisa@gmail.com>
2023-10-19 23:48:19 +0100

    map component edited

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import GoogleMapReact from "google-map-react";
 import { Paper, Typography, useMediaQuery } from "@material-ui/core";
 import { Rating } from "@material-ui/lab";
+import LocationOnOutlinedIcon from "@material-ui/icons/LocationOnOutlined"
 
 import useStyles from "./styles";
 const Map = () => {
