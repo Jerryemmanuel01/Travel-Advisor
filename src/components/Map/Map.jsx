03e863cd3c51b3dd13429100faeb31f16b38c563
Jerryemmanuel01 <jerryemmanuelolisa@gmail.com>
2023-10-19 20:36:47 +0100

    map component created

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -1,9 +1,28 @@
-import React from 'react'
+import React from "react";
+import GoogleMapReact from "google-map-react";
+import { Paper, Typography, useMediaQuery } from "@material-ui/core";
+import { Rating } from "@material-ui/lab";
 
+import useStyles from "./styles";
 const Map = () => {
-  return (
-    <div>Map</div>
-  )
-}
+  const classes = useStyles();
+  const isMobile = useMediaQuery("min-width:600px");
+  const coordinates = { lat: 0, lan: 0 };
 
-export default Map
+  return (
+    <div className={classes.mapContainer}>
+      <GoogleMapReact
+        bootstrapURLKeys={{ key: "" }}
+        defaultCenter={coordinates}
+        center={coordinates}
+        defaultZoom={14}
+        margin={[50, 50, 50, 50]}
+        options={''}
+        onChange={''}
+        onChildClick={''}
+      ></GoogleMapReact>
+    </div>
+  );
+};
+
+export default Map;
