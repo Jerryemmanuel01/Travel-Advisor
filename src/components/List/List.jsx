11e3c71e5825ab344425e6d2c597c72f36dd6034
Jerryemmanuel01 <jerryemmanuelolisa@gmail.com>
2023-10-19 23:52:04 +0100

    list component created

--- src/components/List/List.jsx
+++ src/components/List/List.jsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import React from "react";
+import {
+  CircularProgress,
+  Grid,
+  Typography,
+  InputLabel,
+  MenuItem,
+  FormControl,
+  Select,
+} from "@material-ui/core";
+
+import useStyles from "./styles"
 
 const List = () => {
-  return (
-    <div>List</div>
-  )
-}
+  const classes = useStyles();
+  return <div>List</div>;
+};
 
-export default List
+export default List;
