31933c3182f5eccdf88181af5c42dd0eea56a931
Jerryemmanuel01 <jerryemmanuelolisa@gmail.com>
2023-10-19 01:08:56 +0100

    header component created

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,24 @@
-import { Grid, CssBaseline } from '@mui/material'
-import Header from './components/Header/Header'
-import Map from './components/Map/Map'
-import List from './components/List/List'
+import React from "react";
+import { Grid, CssBaseline } from "@material-ui/core"; 
+import Header from "./components/Header/Header";
+import Map from "./components/Map/Map";
+import List from "./components/List/List";
 
 function App() {
-
   return (
     <>
-    <CssBaseline/>
-    <Header />
-    <Grid container spacing={3} style={{width:"100%"}}>
-      <Grid item xs={12} md={4}>
-        <List />
+      <CssBaseline />
+      <Header />
+      <Grid container spacing={3} style={{ width: "100%" }}>
+        <Grid item xs={12} md={4}>
+          <List />
+        </Grid>
+        <Grid item xs={12} md={8}>
+          <Map />
+        </Grid>
       </Grid>
-      <Grid item xs={12} md={8}>
-        <Map />
-      </Grid>
-    </Grid>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,41 @@
-import React from 'react'
+import React from "react";
+import { Autocomplete } from "@react-google-maps/api";
+import { AppBar, Toolbar, Typography, InputBase, Box } from "@material-ui/core";
+import  SearchIcon  from "@material-ui/icons/Search";
+
+import useStyles from "./styles";
 
 const Header = () => {
-  return (
-    <div>Header</div>
-  )
-}
+  const classes = useStyles();
 
-export default Header
+  return (
+    <AppBar position="static">
+      <Toolbar className={classes.toolbar}>
+        <Typography variant="h5" className={classes.title}>
+          Travel advisor
+        </Typography>
+        <Box display="flex" className={classes.boxCon}>
+          <Typography variant="h6" className={classes.title}>
+            Explore New Places
+          </Typography>
+          {/* <Autocomplete> */}
+          <div className={classes.search}>
+            <div className={classes.searchIcon}>
+              <SearchIcon />
+            </div>
+            <InputBase
+              placeholder="Search..."
+              classes={{ root: classes.inputRoot, input: classes.inputInput }}
+            />
+          </div>
+          {/* </Autocomplete> */}
+        </Box>
+      </Toolbar>
+    </AppBar>
+    // <>
+    //   <h1>headercsa</h1>
+    // </>
+  );
+};
+
+export default Header;
