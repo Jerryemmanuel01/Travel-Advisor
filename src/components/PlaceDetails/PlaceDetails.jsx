08c64ed4c98c66e066c2c28c3292addf6c48fe73
Jerryemmanuel01 <jerryemmanuelolisa@gmail.com>
2023-10-20 23:47:16 +0100

    Card media section added

--- src/components/PlaceDetails/PlaceDetails.jsx
+++ src/components/PlaceDetails/PlaceDetails.jsx
@@ -13,8 +13,23 @@ import LocationOnIcon from "@material-ui/icons/LocationOn";
 import PhoneIcon from "@material-ui/icons/Phone";
 import Rating from "@material-ui/lab/Rating";
 
+import useStyles from './styles';
+
 const PlaceDetails = ({ place }) => {
-  return <h1>{place.name}</h1>;
+  const classes = useStyles();
+  return (
+    <Card elevation={6}>
+      <CardMedia
+        style={{ height: 350 }}
+        image={
+          place.photo
+            ? place.photo.images.large.url
+            : "https://images.unsplash.com/photo-1517248135467-4c7edcad34c4?auto=format&fit=crop&q=80&w=1000&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8cmVzdGF1cmFudHxlbnwwfHwwfHx8MA%3D%3D"
+        }
+        title={place.name}
+      />
+    </Card>
+  );
 };
 
 export default PlaceDetails;
